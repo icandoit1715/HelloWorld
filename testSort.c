216b7ab91866d69ef414337780f8a17b533c3842
dongUbu <justdoit1715@163.com>
2025-04-15 06:57:35 -0700

    添加了测试排序的代码

---
+++ testSort.c
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "sort.h"
+
+using namespace std;
+
+int main()
+{
+	int arr[] = {1, 9, 10, 5, 2, 20,  3, 5, 52, 35, 94};
+	cout << "before sort: ";
+	for(const auto &item : arr)
+					cout << item << " ";
+	insertSort(arr, sizeof(arr) / sizeof(int));
+	cout << endl;
+	cout << "after sort: ";
+	for(const auto &item : arr)
+					cout << item << " ";
+	cout << endl;
+
+	return 0;
+}
